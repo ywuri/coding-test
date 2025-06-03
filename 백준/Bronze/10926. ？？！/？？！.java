3ea079de2fa1201b69b87e55cc2acb0cac120373
유리 <jip03022@naver.com>
2025-06-03 15:13:15 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String nickname = sc.next();
+        String nicknametotal = nickname+"??!";
+        
+        System.out.println(nicknametotal);
+    }
+}
